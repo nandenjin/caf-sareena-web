73df61d5f6ad0d5977010bdc8e62f821ee0c6bce
Kazumi Inada <hello@nandenjin.com>
2024-03-15 01:32:54 +0900

    fix: Small fix for text size

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,11 @@ import LandingInfo from './components/LandingInfo.vue'
 
 <style>
 :root {
-  font-size: 24px;
+  font-size: 20px;
   --margin-page: 2rem;
 
   @media screen and (max-width: 1200px) {
-    font-size: 2vw;
+    font-size: 1.8vw;
   }
 
   @media screen and (max-width: 768px) {
