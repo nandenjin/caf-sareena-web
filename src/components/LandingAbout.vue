babd1fb7a451677a2f098ee36874cbf623d60922
Kazumi Inada <hello@nandenjin.com>
2024-03-17 22:12:29 +0900

    fix: Unmatched border line

--- src/components/LandingAbout.vue
+++ src/components/LandingAbout.vue
@@ -1,13 +1,15 @@
 <template>
-  <section class="font-gothic text-column">
-    <div lang="ja">
-      本展覧会は、公益財団法人現代芸術振興財団の行う「CAF(Contemporary Art
-      Foundation)賞」の最優秀賞の副賞として開催されます。CAF賞とは学生の方々を対象に若手アーティスト育成を目的として2014年から毎年実施しているアートアワードです。最優秀賞者には賞金100万円のほか、副賞として個展開催の機会を提供しております。
-    </div>
-    <div lang="en" style="margin-top: 1.5rem">
-      This exhibition is part of the eighth annual “CAF Award” organized by the
-      Contemporary Art Foundation supporting young art students. The Grand Prix
-      winner is honored with 1 million yen and a solo exhibition.
+  <section>
+    <div class="font-gothic text-column">
+      <p lang="ja">
+        本展覧会は、公益財団法人現代芸術振興財団の行う「CAF(Contemporary Art
+        Foundation)賞」の最優秀賞の副賞として開催されます。CAF賞とは学生の方々を対象に若手アーティスト育成を目的として2014年から毎年実施しているアートアワードです。最優秀賞者には賞金100万円のほか、副賞として個展開催の機会を提供しております。
+      </p>
+      <p lang="en" style="margin-top: 1.5rem">
+        This exhibition is part of the eighth annual “CAF Award” organized by
+        the Contemporary Art Foundation supporting young art students. The Grand
+        Prix winner is honored with 1 million yen and a solo exhibition.
+      </p>
     </div>
   </section>
 </template>
