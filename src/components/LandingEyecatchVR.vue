37e6da6c5dd990f9eb3266eee9ef333ab9c4e24f
Kazumi Inada <hello@nandenjin.com>
2024-03-17 14:13:46 +0900

    fix: Update picture contents

--- src/components/LandingEyecatchVR.vue
+++ src/components/LandingEyecatchVR.vue
@@ -3,13 +3,42 @@
 </template>
 
 <script lang="ts">
-import Image0 from '@/assets/images/image_0.jpg'
-import Image1 from '@/assets/images/image_1.jpg'
-// import Image2 from '@/assets/images/image_2.jpg'
-import Image3 from '@/assets/images/image_3.jpg'
-import Image4 from '@/assets/images/image_4.jpg'
-import Image5 from '@/assets/images/image_5.jpg'
-import Image6 from '@/assets/images/image_6.jpg'
+import Image11 from '@/assets/images/1-1.jpg'
+import Image12 from '@/assets/images/1-2.jpg'
+import Image13 from '@/assets/images/1-3.jpg'
+import Image14 from '@/assets/images/1-4.jpg'
+import Image15 from '@/assets/images/1-5.jpg'
+import Image16 from '@/assets/images/1-6.jpg'
+import Image21 from '@/assets/images/2-1.jpg'
+import Image22 from '@/assets/images/2-2.jpg'
+import Image23 from '@/assets/images/2-3.jpg'
+import Image24 from '@/assets/images/2-4.jpg'
+import Image25 from '@/assets/images/2-5.jpg'
+import Image26 from '@/assets/images/2-6.jpg'
+import Image31 from '@/assets/images/3-1.jpg'
+import Image32 from '@/assets/images/3-2.jpg'
+import Image33 from '@/assets/images/3-3.jpg'
+import Image34 from '@/assets/images/3-4.jpg'
+import Image35 from '@/assets/images/3-5.jpg'
+import Image36 from '@/assets/images/3-6.jpg'
+import Image41 from '@/assets/images/4-1.jpg'
+import Image42 from '@/assets/images/4-2.jpg'
+import Image43 from '@/assets/images/4-3.jpg'
+import Image44 from '@/assets/images/4-4.jpg'
+import Image45 from '@/assets/images/4-5.jpg'
+import Image46 from '@/assets/images/4-6.jpg'
+import Image51 from '@/assets/images/5-1.jpg'
+import Image52 from '@/assets/images/5-2.jpg'
+import Image53 from '@/assets/images/5-3.jpg'
+import Image54 from '@/assets/images/5-4.jpg'
+import Image55 from '@/assets/images/5-5.jpg'
+import Image56 from '@/assets/images/5-6.jpg'
+import Image61 from '@/assets/images/6-1.jpg'
+import Image62 from '@/assets/images/6-2.jpg'
+import Image63 from '@/assets/images/6-3.jpg'
+import Image64 from '@/assets/images/6-4.jpg'
+import Image65 from '@/assets/images/6-5.jpg'
+import Image66 from '@/assets/images/6-6.jpg'
 
 const createPosition = (xz: number, y: number) => {
   const x = Math.cos(xz)
@@ -22,41 +51,51 @@ const pictureFrameConfigs: {
   position: Vector3
   distance: number
   cycleMs: number
+  pictures: string[]
 }[] = [
   {
-    position: createPosition(-0.6, -0.2),
-    distance: 2000,
-    cycleMs: 3000,
-  },
-  {
+    // Slot 1
     position: createPosition(-0.3, 0.2),
     distance: 2000,
     cycleMs: 4000,
+    pictures: [Image11, Image12, Image13, Image14, Image15, Image16],
   },
   {
-    position: createPosition(-0.2, -0.25),
-    distance: 2000,
-    cycleMs: 6000,
-  },
-  {
-    position: createPosition(0, 0),
-    distance: 2500,
-    cycleMs: 3000,
-  },
-  {
+    // Slot 2
     position: createPosition(0.35, 0.15),
     distance: 1500,
     cycleMs: 2000,
+    pictures: [Image21, Image22, Image23, Image24, Image25, Image26],
   },
   {
+    // Slot 3
+    position: createPosition(0, 0),
+    distance: 2500,
+    cycleMs: 3000,
+    pictures: [Image31, Image32, Image33, Image34, Image35, Image36],
+  },
+  {
+    // Slot 4
+    position: createPosition(-0.6, -0.2),
+    distance: 2000,
+    cycleMs: 3000,
+    pictures: [Image41, Image42, Image43, Image44, Image45, Image46],
+  },
+  {
+    // Slot 5
+    position: createPosition(-0.2, -0.25),
+    distance: 2000,
+    cycleMs: 6000,
+    pictures: [Image51, Image52, Image53, Image54, Image55, Image56],
+  },
+  {
+    // Slot 6
     position: createPosition(0.6, -0.15),
     distance: 1500,
     cycleMs: 5000,
+    pictures: [Image61, Image62, Image63, Image64, Image65, Image66],
   },
 ]
-
-/** Array of resolved path to pictures */
-const pictures = [Image0, Image1, Image3, Image4, Image5, Image6]
 </script>
 
 <script setup lang="ts">
@@ -78,6 +117,7 @@ class VRPictureFrame {
   domElement = document.createElement('div')
   css3DObject = new CSS3DObject(this.domElement)
   cycleMs: number
+  pictures: string[] = []
 
   /** Unixtime in ms */
   private nextUpdate: number
@@ -91,7 +131,7 @@ class VRPictureFrame {
   private set index(value) {
     // Create next image DOM
     const nextImageDom = document.createElement('img')
-    nextImageDom.src = pictures[value]
+    nextImageDom.src = this.pictures[value]
     nextImageDom.setAttribute('alt', '')
 
     // When next image is loaded,
@@ -125,7 +165,7 @@ class VRPictureFrame {
     width: number,
     height: number,
     cycleMs: number,
-    initialImageIndex: number = 0
+    pictures: string[]
   ) {
     this.domElement.classList.add('picture-frame')
     this.domElement.style.width = width + 'px'
@@ -133,14 +173,15 @@ class VRPictureFrame {
     this.domElement.style.background = '#eee'
     this.nextUpdate = Date.now() + cycleMs * Math.random()
     this.cycleMs = cycleMs
-    this.index = initialImageIndex
+    this.pictures = pictures
   }
 
   tick() {
-    if (Date.now() > this.nextUpdate) {
-      this.nextUpdate = Date.now() + this.cycleMs
+    const { nextUpdate, cycleMs, pictures, index } = this
+    if (Date.now() > nextUpdate) {
+      this.nextUpdate = Date.now() + cycleMs
       this.domElement.style.background = `url(${pictures[this.index]})`
-      this.index = (this.index + 1) % pictures.length
+      this.index = (index + 1) % pictures.length
     }
   }
 }
@@ -218,9 +259,9 @@ onMounted(() => {
   renderer.value = tRenderer
 
   // Init picture frames
-  let i = 0
-  for (const { position, distance, cycleMs } of pictureFrameConfigs) {
-    const frame = new VRPictureFrame(480, 320, cycleMs, i % pictures.length)
+  for (let i = 0; i < pictureFrameConfigs.length; i++) {
+    const { position, distance, cycleMs, pictures } = pictureFrameConfigs[i]
+    const frame = new VRPictureFrame(480, 320, cycleMs, pictures)
 
     // Positioning
     frame.css3DObject.position.copy(position).setLength(distance)
@@ -229,8 +270,6 @@ onMounted(() => {
     // Add to scene
     frames.value.push(frame)
     tScene.add(frame.css3DObject)
-
-    i++
   }
 
   // Init camera
