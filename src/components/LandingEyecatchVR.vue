1469269742cafad07ac429be6a7ea139fbf6f2b2
Kazumi Inada <hello@nandenjin.com>
2024-03-17 15:46:12 +0900

    feat: Implement loading indicator

--- src/components/LandingEyecatchVR.vue
+++ src/components/LandingEyecatchVR.vue
@@ -1,107 +1,15 @@
 <template>
-  <div ref="rendererContainer"></div>
+  <div class="vr-root" :class="{ 'is-loading': isLoading }">
+    <div ref="rendererContainer" class="renderer"></div>
+    <div class="loading"></div>
+  </div>
 </template>
 
-<script lang="ts">
-import Image11 from '@/assets/images/1-1.jpg'
-import Image12 from '@/assets/images/1-2.jpg'
-import Image13 from '@/assets/images/1-3.jpg'
-import Image14 from '@/assets/images/1-4.jpg'
-import Image15 from '@/assets/images/1-5.jpg'
-import Image16 from '@/assets/images/1-6.jpg'
-import Image21 from '@/assets/images/2-1.jpg'
-import Image22 from '@/assets/images/2-2.jpg'
-import Image23 from '@/assets/images/2-3.jpg'
-import Image24 from '@/assets/images/2-4.jpg'
-import Image25 from '@/assets/images/2-5.jpg'
-import Image26 from '@/assets/images/2-6.jpg'
-import Image31 from '@/assets/images/3-1.jpg'
-import Image32 from '@/assets/images/3-2.jpg'
-import Image33 from '@/assets/images/3-3.jpg'
-import Image34 from '@/assets/images/3-4.jpg'
-import Image35 from '@/assets/images/3-5.jpg'
-import Image36 from '@/assets/images/3-6.jpg'
-import Image41 from '@/assets/images/4-1.jpg'
-import Image42 from '@/assets/images/4-2.jpg'
-import Image43 from '@/assets/images/4-3.jpg'
-import Image44 from '@/assets/images/4-4.jpg'
-import Image45 from '@/assets/images/4-5.jpg'
-import Image46 from '@/assets/images/4-6.jpg'
-import Image51 from '@/assets/images/5-1.jpg'
-import Image52 from '@/assets/images/5-2.jpg'
-import Image53 from '@/assets/images/5-3.jpg'
-import Image54 from '@/assets/images/5-4.jpg'
-import Image55 from '@/assets/images/5-5.jpg'
-import Image56 from '@/assets/images/5-6.jpg'
-import Image61 from '@/assets/images/6-1.jpg'
-import Image62 from '@/assets/images/6-2.jpg'
-import Image63 from '@/assets/images/6-3.jpg'
-import Image64 from '@/assets/images/6-4.jpg'
-import Image65 from '@/assets/images/6-5.jpg'
-import Image66 from '@/assets/images/6-6.jpg'
-
-const createPosition = (xz: number, y: number) => {
-  const x = Math.cos(xz)
-  const z = Math.sin(xz)
-  return new Vector3(x, y, z)
-}
-
-/** Configuration for picture frames */
-const pictureFrameConfigs: {
-  position: Vector3
-  distance: number
-  cycleMs: number
-  pictures: string[]
-}[] = [
-  {
-    // Slot 1
-    position: createPosition(-0.3, 0.2),
-    distance: 2000,
-    cycleMs: 4000,
-    pictures: [Image11, Image12, Image13, Image14, Image15, Image16],
-  },
-  {
-    // Slot 2
-    position: createPosition(0.35, 0.15),
-    distance: 1500,
-    cycleMs: 2000,
-    pictures: [Image21, Image22, Image23, Image24, Image25, Image26],
-  },
-  {
-    // Slot 3
-    position: createPosition(0, 0),
-    distance: 2500,
-    cycleMs: 3000,
-    pictures: [Image31, Image32, Image33, Image34, Image35, Image36],
-  },
-  {
-    // Slot 4
-    position: createPosition(-0.6, -0.2),
-    distance: 2000,
-    cycleMs: 3000,
-    pictures: [Image41, Image42, Image43, Image44, Image45, Image46],
-  },
-  {
-    // Slot 5
-    position: createPosition(-0.2, -0.25),
-    distance: 2000,
-    cycleMs: 6000,
-    pictures: [Image51, Image52, Image53, Image54, Image55, Image56],
-  },
-  {
-    // Slot 6
-    position: createPosition(0.6, -0.15),
-    distance: 1500,
-    cycleMs: 5000,
-    pictures: [Image61, Image62, Image63, Image64, Image65, Image66],
-  },
-]
-</script>
-
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue'
-import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/Addons.js'
-import { Camera, PerspectiveCamera, Scene, Vector2, Vector3 } from 'three'
+import { computed, onMounted, onUnmounted, reactive, ref } from 'vue'
+import { CSS3DRenderer } from 'three/addons'
+import { Camera, PerspectiveCamera, Scene, Vector2 } from 'three'
+import { PictureAsset, VRPictureFrame, pictures } from '../lib/pictures'
 
 const frames = ref<VRPictureFrame[]>([])
 
@@ -112,79 +20,11 @@ const camera = ref<Camera>(new PerspectiveCamera(40))
 const isMounted = ref(false)
 const pointerPosition = ref(new Vector2())
 const pointerPositionSmoothed = ref(new Vector2())
-
-class VRPictureFrame {
-  domElement = document.createElement('div')
-  css3DObject = new CSS3DObject(this.domElement)
-  cycleMs: number
-  pictures: string[] = []
-
-  /** Unixtime in ms */
-  private nextUpdate: number
-
-  private currentImageDom: HTMLImageElement | null = null
-
-  private _index = 0
-  private get index() {
-    return this._index
-  }
-  private set index(value) {
-    // Create next image DOM
-    const nextImageDom = document.createElement('img')
-    nextImageDom.src = this.pictures[value]
-    nextImageDom.setAttribute('alt', '')
-
-    // When next image is loaded,
-    nextImageDom.addEventListener('load', () => {
-      const current = this.currentImageDom
-
-      // If current image exists,
-      if (current) {
-        // Remove current image after transition
-        current.addEventListener('transitionend', () => {
-          this.domElement.removeChild(current!)
-          this.currentImageDom = nextImageDom
-          nextImageDom.classList.add('is-active')
-        })
-        current.classList.add('is-being-removed')
-      }
-
-      // If this is the first image,
-      else {
-        nextImageDom.classList.add('is-active')
-        this.currentImageDom = nextImageDom
-      }
-
-      // Add next image to the dom
-      this.domElement.appendChild(nextImageDom)
-    })
-    this._index = value
-  }
-
-  constructor(
-    width: number,
-    height: number,
-    cycleMs: number,
-    pictures: string[]
-  ) {
-    this.domElement.classList.add('picture-frame')
-    this.domElement.style.width = width + 'px'
-    this.domElement.style.height = height + 'px'
-    this.domElement.style.background = '#eee'
-    this.nextUpdate = Date.now() + cycleMs * Math.random()
-    this.cycleMs = cycleMs
-    this.pictures = pictures
-  }
-
-  tick() {
-    const { nextUpdate, cycleMs, pictures, index } = this
-    if (Date.now() > nextUpdate) {
-      this.nextUpdate = Date.now() + cycleMs
-      this.domElement.style.background = `url(${pictures[this.index]})`
-      this.index = (index + 1) % pictures.length
-    }
-  }
-}
+const loadingState = reactive({
+  total: 0,
+  loaded: 0,
+})
+const isLoading = computed(() => loadingState.total > loadingState.loaded)
 
 /** Resize renderer with window size */
 const handleCanvasResize = () => {
@@ -243,11 +83,37 @@ const render = () => {
   requestAnimationFrame(render)
 }
 
-onMounted(() => {
+onMounted(async () => {
   if (!rendererContainer.value) {
     console.error('rendererContainer is not found')
     return
   }
+
+  loadingState.loaded = 0
+  loadingState.total = 0
+
+  const loadingTasks: Promise<PictureAsset>[] = []
+  for (const picture of pictures) {
+    loadingState.total += picture.urls.length
+
+    loadingTasks.push(
+      Promise.all(
+        picture.urls.map((url: string) =>
+          fetch(url)
+            .then((res) => res.blob())
+            .then((blob) => {
+              loadingState.loaded++
+              return URL.createObjectURL(blob)
+            })
+        )
+      ).then((urls) => ({
+        ...picture,
+        urls,
+      }))
+    )
+  }
+
+  const pictureFrameConfigs = await Promise.all(loadingTasks)
 
   const tScene = scene.value,
     tCamera = camera.value
@@ -260,8 +126,10 @@ onMounted(() => {
 
   // Init picture frames
   for (let i = 0; i < pictureFrameConfigs.length; i++) {
-    const { position, distance, cycleMs, pictures } = pictureFrameConfigs[i]
-    const frame = new VRPictureFrame(480, 320, cycleMs, pictures)
+    const { position, distance, cycleMs, urls } = pictureFrameConfigs[i]
+    const frame = new VRPictureFrame(480, 320, cycleMs)
+    frame.setPictures(urls)
+    frame.setIndex(Math.floor(urls.length * Math.random()))
 
     // Positioning
     frame.css3DObject.position.copy(position).setLength(distance)
@@ -299,6 +167,58 @@ onUnmounted(() => {
   window.removeEventListener('deviceorientation', handleOrientation)
 })
 </script>
+
+<style scoped>
+.vr-root {
+  position: relative;
+
+  .renderer {
+    transition: opacity 0.3s;
+  }
+
+  .loading {
+    position: absolute;
+    bottom: 2rem;
+    right: 2rem;
+    transition: opacity 0.3s;
+
+    &::before {
+      content: '';
+      display: block;
+      width: 1rem;
+      height: 1rem;
+      border: 0.3rem solid #000;
+      border-color: #000 #000 #000 transparent;
+      border-radius: 50%;
+      animation-name: loading-rotate;
+      animation-duration: 0.5s;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+    }
+  }
+
+  &.is-loading {
+    .renderer {
+      opacity: 0;
+    }
+  }
+
+  &:not(.is-loading) {
+    .loading {
+      opacity: 0;
+    }
+  }
+}
+
+@keyframes loading-rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
 
 <style>
 .picture-frame {
---
+++ src/lib/pictures.ts
@@ -0,0 +1,165 @@
+import { Vector3 } from 'three'
+import { CSS3DObject } from 'three/addons'
+
+import Image11 from '@/assets/images/1-1.jpg'
+import Image12 from '@/assets/images/1-2.jpg'
+import Image13 from '@/assets/images/1-3.jpg'
+import Image14 from '@/assets/images/1-4.jpg'
+import Image15 from '@/assets/images/1-5.jpg'
+import Image16 from '@/assets/images/1-6.jpg'
+import Image21 from '@/assets/images/2-1.jpg'
+import Image22 from '@/assets/images/2-2.jpg'
+import Image23 from '@/assets/images/2-3.jpg'
+import Image24 from '@/assets/images/2-4.jpg'
+import Image25 from '@/assets/images/2-5.jpg'
+import Image26 from '@/assets/images/2-6.jpg'
+import Image31 from '@/assets/images/3-1.jpg'
+import Image32 from '@/assets/images/3-2.jpg'
+import Image33 from '@/assets/images/3-3.jpg'
+import Image34 from '@/assets/images/3-4.jpg'
+import Image35 from '@/assets/images/3-5.jpg'
+import Image36 from '@/assets/images/3-6.jpg'
+import Image41 from '@/assets/images/4-1.jpg'
+import Image42 from '@/assets/images/4-2.jpg'
+import Image43 from '@/assets/images/4-3.jpg'
+import Image44 from '@/assets/images/4-4.jpg'
+import Image45 from '@/assets/images/4-5.jpg'
+import Image46 from '@/assets/images/4-6.jpg'
+import Image51 from '@/assets/images/5-1.jpg'
+import Image52 from '@/assets/images/5-2.jpg'
+import Image53 from '@/assets/images/5-3.jpg'
+import Image54 from '@/assets/images/5-4.jpg'
+import Image55 from '@/assets/images/5-5.jpg'
+import Image56 from '@/assets/images/5-6.jpg'
+import Image61 from '@/assets/images/6-1.jpg'
+import Image62 from '@/assets/images/6-2.jpg'
+import Image63 from '@/assets/images/6-3.jpg'
+import Image64 from '@/assets/images/6-4.jpg'
+import Image65 from '@/assets/images/6-5.jpg'
+import Image66 from '@/assets/images/6-6.jpg'
+
+const createPosition = (xz: number, y: number) => {
+  const x = Math.cos(xz)
+  const z = Math.sin(xz)
+  return new Vector3(x, y, z)
+}
+
+export type PictureAsset = {
+  position: Vector3
+  distance: number
+  cycleMs: number
+  urls: string[]
+}
+
+/** Configuration for picture frames */
+export const pictures: PictureAsset[] = [
+  {
+    // Slot 1
+    position: createPosition(-0.3, 0.2),
+    distance: 2000,
+    cycleMs: 4000,
+    urls: [Image11, Image12, Image13, Image14, Image15, Image16],
+  },
+  {
+    // Slot 2
+    position: createPosition(0.35, 0.15),
+    distance: 1500,
+    cycleMs: 2000,
+    urls: [Image21, Image22, Image23, Image24, Image25, Image26],
+  },
+  {
+    // Slot 3
+    position: createPosition(0, 0),
+    distance: 2500,
+    cycleMs: 3000,
+    urls: [Image31, Image32, Image33, Image34, Image35, Image36],
+  },
+  {
+    // Slot 4
+    position: createPosition(-0.6, -0.2),
+    distance: 2000,
+    cycleMs: 3000,
+    urls: [Image41, Image42, Image43, Image44, Image45, Image46],
+  },
+  {
+    // Slot 5
+    position: createPosition(-0.2, -0.25),
+    distance: 2000,
+    cycleMs: 6000,
+    urls: [Image51, Image52, Image53, Image54, Image55, Image56],
+  },
+  {
+    // Slot 6
+    position: createPosition(0.6, -0.15),
+    distance: 1500,
+    cycleMs: 5000,
+    urls: [Image61, Image62, Image63, Image64, Image65, Image66],
+  },
+]
+
+export class VRPictureFrame {
+  domElement = document.createElement('div')
+  css3DObject = new CSS3DObject(this.domElement)
+  cycleMs: number
+  pictures: string[] = []
+
+  /** Unixtime in ms */
+  private nextUpdate: number
+
+  private currentImageDom: HTMLImageElement | null = null
+
+  private index: number = -1
+
+  constructor(width: number, height: number, cycleMs: number) {
+    this.domElement.classList.add('picture-frame')
+    this.domElement.style.width = width + 'px'
+    this.domElement.style.height = height + 'px'
+    this.domElement.style.background = '#eee'
+    this.nextUpdate = Date.now() + cycleMs * Math.random()
+    this.cycleMs = cycleMs
+  }
+
+  setPictures(pictures: string[]) {
+    this.pictures = pictures
+  }
+  setIndex(value: number) {
+    // Create next image DOM
+    const nextImageDom = document.createElement('img')
+    nextImageDom.src = this.pictures[value]
+    nextImageDom.setAttribute('alt', '')
+
+    // When next image is loaded,
+    nextImageDom.addEventListener('load', () => {
+      const current = this.currentImageDom
+
+      // If current image exists,
+      if (current) {
+        // Remove current image after transition
+        current.addEventListener('transitionend', () => {
+          this.domElement.removeChild(current!)
+          this.currentImageDom = nextImageDom
+          nextImageDom.classList.add('is-active')
+        })
+        current.classList.add('is-being-removed')
+      }
+
+      // If this is the first image,
+      else {
+        nextImageDom.classList.add('is-active')
+        this.currentImageDom = nextImageDom
+      }
+
+      // Add next image to the dom
+      this.domElement.appendChild(nextImageDom)
+    })
+    this.index = value
+  }
+
+  tick() {
+    const { nextUpdate, cycleMs, pictures, index } = this
+    if (Date.now() > nextUpdate) {
+      this.nextUpdate = Date.now() + cycleMs
+      this.index = (index + 1) % pictures.length
+    }
+  }
+}
