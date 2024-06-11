af3e8ee84dc69c2c916622f8a49885c64e4fe0ed
Kazumi Inada <hello@nandenjin.com>
2024-06-12 01:15:07 +0900

    feat: Add interview video (#7)

---
+++ src/components/AppearByClick.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="clicked">
+    <slot></slot>
+  </div>
+  <div v-else @click="clicked = true">
+    <slot name="placeholder"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const clicked = ref(false)
+</script>
